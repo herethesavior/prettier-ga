30690ccaf5c048cac25faa16e7446f0232300e5d
Ryan Joshua Dela Cruz <83619637+herethesavior@users.noreply.github.com>
2024-03-18 16:42:20 +0800

    Update continuationAuraController.js

--- force-app/main/default/aura/continuationAura/continuationAuraController.js
+++ force-app/main/default/aura/continuationAura/continuationAuraController.js
@@ -2,7 +2,7 @@
   callContinuation: function (cmp) {
     var action = cmp.get("c.startRequest");
     action.setCallback(this, function (response) {
-      var state = response.getState();
+  var state = response.getState();
       if (state === "SUCCESS") {
         console.log(
           "From server: " +
