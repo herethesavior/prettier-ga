631714f3534d9a089792c43f4cc454fe050ee860
Ryan Joshua Dela Cruz <ryan.joshua.dc@gmail.com>
2025-03-24 06:13:47 +0000

    chore: format source code with Prettier

--- force-app/main/default/aura/continuationAura/continuationAuraController.js
+++ force-app/main/default/aura/continuationAura/continuationAuraController.js
@@ -5,13 +5,13 @@
       var state = response.getState();
       if (state === "SUCCESS") {
         console.log("From server: " + response.getReturnValue() + "\n" + JSON.stringify(response.getReturnValue()));
-} else if (state === "INCOMPLETE") {
-  alert("Continuation action is INCOMPLETE");
-} else if (state === "ERROR") {
+      } else if (state === "INCOMPLETE") {
+        alert("Continuation action is INCOMPLETE");
+      } else if (state === "ERROR") {
         var errors = response.getError();
         if (errors) {
-      if (errors[0] && errors[0].message) {
-        console.log("Error message: " + errors[0].message);
+          if (errors[0] && errors[0].message) {
+            console.log("Error message: " + errors[0].message);
           }
         } else {
           console.log("Unknown error");
