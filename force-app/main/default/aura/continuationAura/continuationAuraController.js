4118e5727adcb10d913c84c63a936a2e7fa8223d
Ryan Joshua Dela Cruz <ryan.joshua.dc@gmail.com>
2025-03-24 13:50:31 +0800

    test: added unformatted files

--- force-app/main/default/aura/continuationAura/continuationAuraController.js
+++ force-app/main/default/aura/continuationAura/continuationAuraController.js
@@ -1,19 +1,19 @@
 ({
   callContinuation: function (cmp) {
     var action = cmp.get("c.startRequest");
-    action.setCallback(this, function (response) {
-      var state = response.getState();
-      if (state === "SUCCESS") {
-        console.log("From server: " + response.getReturnValue() + "\n" + JSON.stringify(response.getReturnValue()));
-      } else if (state === "INCOMPLETE") {
-        alert("Continuation action is INCOMPLETE");
-      } else if (state === "ERROR") {
-        var errors = response.getError();
-        if (errors) {
-          if (errors[0] && errors[0].message) {
-            console.log("Error message: " + errors[0].message);
-          }
-        } else {
+  action.setCallback(this, function (response) {
+      var state = response.getState();   
+      if (state === "SUCCESS") {   
+  console.log("From server: " + response.getReturnValue() + "\n" + JSON.stringify(response.getReturnValue()));
+} else if (state === "INCOMPLETE") {
+  alert("Continuation action is INCOMPLETE");
+      } else if (state === "ERROR"){
+        var errors = response.getError();   
+  if (errors) {
+    if (errors[0] && errors[0].message) {
+      console.log("Error message: " + errors[0].message);
+    }
+  } else {
           console.log("Unknown error");
         }
       }
